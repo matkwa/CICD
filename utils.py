bc005c8bfdd8f88bb158bd449ad30920acada60a
Mateusz Kwapisz <matkwa2283@pwr.edu.pl>
2026-05-05 17:50:55 +0200

    add calculator

--- utils.py
+++ utils.py
@@ -1,12 +1,31 @@
-# Exemplary calculator functions
- def add ( a : int , b : int ) -> int :
- return a + b
+"""
+Utility module providing basic arithmetic operations.
+"""
 
- def subtract ( a : int , b : int ) -> int :
- return a - b
 
- def multiply ( a : int , b : int ) -> int :
- return a * b
+def add(a: int, b: int) -> int:
+    """
+    Add two integers.
+    """
+    return a + b
 
- def divide ( a : int , b : int ) -> float :
- return a / b
+
+def subtract(a: int, b: int) -> int:
+    """
+    Subtract the second integer from the first.
+    """
+    return a - b
+
+
+def multiply(a: int, b: int) -> int:
+    """
+    Multiply two integers.
+    """
+    return a * b
+
+
+def divide(a: int, b: int) -> float:
+    """
+    Divide the first number by the second.
+    """
+    return a / b
